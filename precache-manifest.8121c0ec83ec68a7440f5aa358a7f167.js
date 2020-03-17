cd597fea9de143a2250aaa854b879a7d72a95a65
KevinH48264 <1kevin.huang@gmail.com>
2020-03-16 21:09:36 -0400

    Updates

---
+++ precache-manifest.8121c0ec83ec68a7440f5aa358a7f167.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1387949ea8d319b99a06ab051299681",
+    "url": "/project-corona/index.html"
+  },
+  {
+    "revision": "e1a7ad120e272b7615b9",
+    "url": "/project-corona/static/js/2.f4d6bf36.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/project-corona/static/js/2.f4d6bf36.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6625de08b29c21ff0363",
+    "url": "/project-corona/static/js/main.6055683d.chunk.js"
+  },
+  {
+    "revision": "5c8364263acb2f4906f4",
+    "url": "/project-corona/static/js/runtime-main.e79393a8.js"
+  }
+]);
